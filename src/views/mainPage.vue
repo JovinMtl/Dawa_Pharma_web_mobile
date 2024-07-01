d55c6063132d4e820880247a9a1b6899f007a27d
JovinMtl <nsanzumukizath@gmail.com>
2024-07-01 12:30:05 +0200

    updated the correct path of vendu and dispo

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -3,12 +3,12 @@
       <ion-tabs>
         <ion-router-outlet></ion-router-outlet>
         <ion-tab-bar slot="bottom">
-          <ion-tab-button tab="tab1" href="/dispo">
+          <ion-tab-button tab="tab1" href="/main/dispo">
             <ion-icon aria-hidden="true" :icon="triangle" />
             <ion-label>Dispo</ion-label>
           </ion-tab-button>
   
-          <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-tab-button tab="tab2" href="/main/vendu">
             <ion-icon aria-hidden="true" :icon="ellipse" />
             <ion-label>Vendu</ion-label>
           </ion-tab-button>
