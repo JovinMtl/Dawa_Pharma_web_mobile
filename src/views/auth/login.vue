1daaa79770d61018a4209b321018bc3d52103812
JovinMtl <nsanzumukizath@gmail.com>
2024-07-06 12:20:57 +0200

    Turned into an Ionic component.

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,31 +1,90 @@
 <template>
-    <div class="LogContainer">
-        ICi authentification
-        <div class="authentif">
-            <div class="username">
-                <label for="input">Username</label> <br>
-                <input type="text" style="padding: 15px 30px; 
-                    border-radius: 15px;border-style: hidden;
-                    background-color: rgb(28, 35, 49);">
+    <ion-page>
+        <ion-content>
+            <div class="LogContainer" id="authe">
+                ICi authentification
+                <div class="authentif">
+                    <div class="username">
+                        <label for="input">Username</label> <br>
+                        <input v-model="username" type="text" style="padding: 15px 30px; 
+                            border-radius: 15px;border-style: hidden;
+                            background-color: rgb(28, 35, 49);">
+                    </div>
+                    <div class="username">
+                        <label for="input">Password</label> <br>
+                        <input v-model="password" type="password" style="padding: 15px 30px; 
+                            border-radius: 15px; border-spacing: 5px; 
+                            border-style: hidden; background-color: rgb(28, 35, 49);">
+                    </div>
+                    <div class="confirmationContainer">
+                        <!-- <div class="one" style="border-right: 1px solid black;">s'inscrire</div>
+                        <div class="one">se connecter</div> -->
+                        <!-- se connecter -->
+                    </div>
+                    <div @click="login_hook" class="enter">
+                        <div class="btn">se connecter</div>
+                    </div>
+                </div>
             </div>
-            <div class="username">
-                <label for="input">Password</label> <br>
-                <input type="password" style="padding: 15px 30px; 
-                    border-radius: 15px; border-spacing: 5px; 
-                    border-style: hidden; background-color: rgb(28, 35, 49);">
-            </div>
-            <div class="confirmationContainer">
-                <!-- <div class="one" style="border-right: 1px solid black;">s'inscrire</div>
-                <div class="one">se connecter</div> -->
-                <!-- se connecter -->
-            </div>
-            <div class="enter">
-                <div class="btn">se connecter</div>
-            </div>
-        </div>
-    </div>
+        </ion-content>
+    </ion-page>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
+import { 
+    IonContent, IonPage
+} from '@ionic/vue';
+// import { useStore } from 'vuex'
+import { useUserStore } from '../../store/user.js'
+import { baseURL } from '../../store/host'
+import axios from 'axios'
+
+const store = useUserStore()
+const {getUsername,getAccessToken,getRefreshToken,
+    setUsername,setAccessToken, setRefreshToken} = store
+const router = useRouter()
+const username = ref(null)
+const password = ref(null)
+
+let logs = ''
+
+
+const axiosInstance = axios.create({
+  baseURL: '//127.0.0.1:8002', // Base URL for your API
+  headers: {
+    'Authorization': 'Bearer YOUR_ACCESS_TOKEN' // Add your authorization token here
+  }
+});
+
+const login_hook = ()=>{
+    const base = '//127.0.0.1:8002'
+    const prefix = "api/login/"
+
+    axios.post(`${baseURL}/${prefix}`,{"username": username.value,
+        "password":password.value}
+       ).then((response) => {
+        setUsername(username.value)
+        setAccessToken(response.data.access)
+        setRefreshToken(response.data.refresh)
+        localStorage.setItem('username', username.value)
+        localStorage.setItem('accessToken', response.data.access)
+        localStorage.setItem('refreshToken', response.data.refresh)
+        // hide_authe()
+        router.push('/home')
+       }).catch((error) => {
+         logs = error.response.data
+       })
+}
+// const hide_authe = ()=>{
+//     const container = document.getElementById('authe')
+//     container.style.display = 'none'
+// }
+
+// watch(data, (value)=>{
+//     console.log("The LOGIN component got: ", value)
+// })
+</script>
 <style scoped lang="scss">
 @media screen and (max-width: 400px) {
     .LogContainer{
@@ -52,6 +111,7 @@
             box-shadow: 0 0 45px black;
             text-align: center;
             align-content: center;
+            // z-index: -5;
 
             .username{
                 margin: 10px 50px;
@@ -93,7 +153,7 @@
                     border-radius: 15px;
                     justify-content: center;
                     align-items: center;
-                    // border: 1px solid white;
+                    cursor: pointer;
                     box-shadow: 0 0 5px white;
                     align-content: center;
                     background: linear-gradient(to right,black,rgb(99, 97, 97), white, gray);
