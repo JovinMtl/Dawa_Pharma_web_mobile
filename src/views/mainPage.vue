4448cc420b4305a2fefa801abeec00088182c650
JovinMtl <nsanzumukizath@gmail.com>
2024-08-02 13:53:21 +0200

    replaced the Achat icon

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -18,7 +18,9 @@
           </ion-tab-button>
   
           <ion-tab-button tab="tab3" href="/main/achats">
-            <ion-icon aria-hidden="true" :icon="square" />
+            <!-- <ion-icon aria-hidden="true" :icon="square" /> -->
+            <Icon icon="arcticons:purchased-apps"     
+            inline="true" width="30" height="30"/>
             <ion-label>Achats</ion-label>
           </ion-tab-button>
   
