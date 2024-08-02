94c9077df14bcf534492ea07522e0c6cee684a46
JovinMtl <nsanzumukizath@gmail.com>
2024-08-02 14:06:31 +0200

    removing the commented code for old icons

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -4,35 +4,30 @@
         <ion-router-outlet></ion-router-outlet>
         <ion-tab-bar slot="bottom">
           <ion-tab-button tab="tab1" href="/main/dispo">
-            <!-- <ion-icon aria-hidden="true" :icon="triangle" /> -->
             <Icon icon="healthicons:pharmacy-outline"  
             inline="true" width="30" height="30"/>
             <ion-label>Dispo</ion-label>
           </ion-tab-button>
   
           <ion-tab-button tab="tab2" href="/main/vendu">
-            <!-- <ion-icon aria-hidden="true" :icon="ellipse"/> -->
             <Icon icon="ep:sold-out"    
             inline="true" width="30" height="30"/>
             <ion-label>Vendu</ion-label>
           </ion-tab-button>
   
           <ion-tab-button tab="tab3" href="/main/achats">
-            <!-- <ion-icon aria-hidden="true" :icon="square" /> -->
             <Icon icon="arcticons:purchased-apps"     
             inline="true" width="30" height="30"/>
             <ion-label>Achats</ion-label>
           </ion-tab-button>
   
           <ion-tab-button tab="tab4" href="/main/rotation">
-            <!-- <ion-icon aria-hidden="true" :icon="addOutline" /> -->
             <Icon icon="hugeicons:recycle-01"      
             width="30" height="30"/>
             <ion-label>Rotation</ion-label>
           </ion-tab-button>
   
           <ion-tab-button tab="tab5" href="/main/plus">
-            <!-- <ion-icon aria-hidden="true" :icon="micOffCircleOutline" /> -->
             <Icon icon="icon-park-twotone:more-app"   
             inline="true" width="30" height="30"/>
             <ion-label>Plus</ion-label>
@@ -44,7 +39,6 @@
   
   <script setup lang="ts">
   import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-  import { ellipse, square, triangle, addOutline, micOffCircleOutline } from 'ionicons/icons';
   import { Icon } from '@iconify/vue';
   </script>
   
