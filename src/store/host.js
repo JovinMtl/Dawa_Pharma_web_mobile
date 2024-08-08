ea6f7c509a685c4863a72ee4080cf905f71f4069
JovinMtl <nsanzumukizath@gmail.com>
2024-08-08 16:01:22 +0200

    back on the remote server

--- src/store/host.js
+++ src/store/host.js
@@ -1,5 +1,5 @@
 
 // This the server to connect to.
 
-// export const baseURL = "https://muteule.pythonanywhere.com"
-export const baseURL = "//127.0.0.1:8002"
+export const baseURL = "https://muteule.pythonanywhere.com"
+// export const baseURL = "//127.0.0.1:8002"
